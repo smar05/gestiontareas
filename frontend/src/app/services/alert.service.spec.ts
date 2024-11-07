1b52b6246f6bd34a7c79457ebb00266457e567e2
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-11-07 11:33:07 -0500

    feature: Pruebas unitarias del front

--- frontend/src/app/services/alert.service.spec.ts
+++ frontend/src/app/services/alert.service.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-
+import Swal, { SweetAlertIcon, SweetAlertResult } from 'sweetalert2';
 import { AlertService } from './alert.service';
 
 describe('AlertService', () => {
-  let service: AlertService;
+  let service: AlertService = new AlertService();
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +12,54 @@ describe('AlertService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call Swal.fire with correct parameters', async () => {
+    const sweetAlertSpy = spyOn(Swal, 'fire').and.returnValue(
+      Promise.resolve({} as SweetAlertResult)
+    );
+
+    const title = 'Test Title';
+    const text = 'Test Text';
+    const icon: SweetAlertIcon = 'success';
+
+    await service.basicAlert(title, text, icon);
+
+    expect(sweetAlertSpy).toHaveBeenCalledWith({
+      title,
+      text,
+      icon,
+      confirmButtonText: 'Ok',
+    } as any);
+  });
+
+  it('should return a SweetAlertResult from Swal.fire', async () => {
+    const sweetAlertResult: SweetAlertResult<any> = {
+      isConfirmed: true,
+      value: 'Test value',
+    } as any;
+    spyOn(Swal, 'fire').and.returnValue(Promise.resolve(sweetAlertResult));
+
+    const result = await service.basicAlert(
+      'Test Title',
+      'Test Text',
+      'success'
+    );
+
+    expect(result).toEqual(sweetAlertResult);
+  });
+
+  it('should resolve with SweetAlertResult when alert is confirmed', async () => {
+    spyOn(Swal, 'fire').and.returnValue(
+      Promise.resolve({ isConfirmed: true } as any)
+    );
+
+    const result = await service.basicAlert(
+      'Test Title',
+      'Test Text',
+      'success'
+    );
+
+    expect(result.isConfirmed).toBeTrue();
   });
 });
--- frontend/src/app/services/api.service.spec.ts
+++ frontend/src/app/services/api.service.spec.ts
@@ -1,16 +1,106 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { environment } from 'src/environments/environment';
+import { ITask } from '../interfaces/i-task';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
+    });
+
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch tasks from API', () => {
+    const mockTasks: ITask[] = [
+      {
+        id: 1,
+        title: 'Task 1',
+        description: 'Task 1 description',
+        date: '2024-11-07',
+      },
+      {
+        id: 2,
+        title: 'Task 2',
+        description: 'Task 2 description',
+        date: '2024-11-07',
+      },
+    ];
+
+    service.getTasks().subscribe((tasks) => {
+      expect(tasks.length).toBe(2);
+      expect(tasks).toEqual(mockTasks);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/tasks`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTasks);
+  });
+
+  it('should create a new task', () => {
+    const newTask: ITask = {
+      id: 3,
+      title: 'New Task',
+      description: 'New task description',
+      date: '2024-11-07',
+    };
+
+    service.newTask(newTask).subscribe((response) => {
+      expect(response).toEqual(newTask);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/tasks`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newTask);
+    req.flush(newTask);
+  });
+
+  it('should delete a task', () => {
+    const taskId: number = 1;
+
+    service.deleteTask(taskId).subscribe((response) => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/tasks/${taskId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
+
+  it('should update a task', () => {
+    const taskId = 1;
+    const updatedTask: ITask = {
+      id: 1,
+      title: 'Updated Task',
+      description: 'Updated description',
+      date: '2024-11-07',
+    };
+
+    service.editTask(taskId, updatedTask).subscribe((response) => {
+      expect(response).toEqual(updatedTask);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/tasks/${taskId}`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(updatedTask);
+    req.flush(updatedTask);
   });
 });
