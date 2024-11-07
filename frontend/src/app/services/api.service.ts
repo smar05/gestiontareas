da3b8ddcafedf522fd45e06b042af8f9cf818ace
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-11-07 02:22:42 -0500

    feature: Enviroments

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ITask } from '../interfaces/i-task';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private backUrl: string = 'http://localhost:8080';
+  private backUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
